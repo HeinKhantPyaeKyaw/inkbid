635931f8250ef167639a614850c37394e42df714
🐽 <heinkhant.pyae@innovasive.co.th>
2025-07-20 13:32:05 +0700

    🐽update: global colors and fonts & init navbars

---
+++ frontend/src/presentation/components/navbar/navbar_primary.tsx
@@ -0,0 +1,23 @@
+export const NavbarPrimary = () => {
+  //---------------------
+  //   CONST
+  //---------------------
+  const title = "Primary Navbar";
+
+  //---------------------
+  //   HANDLE
+  //---------------------
+
+  //---------------------
+  //   RENDER
+  //---------------------
+  return (
+    <nav>
+      <></>
+      <div>INKBID</div>
+      <div>
+        <i className="fa fa-bars" aria-hidden="true" />
+      </div>
+    </nav>
+  );
+};
---
+++ frontend/src/presentation/landing_page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const LandingPage = () => {
+  //---------------------
+  //   CONST
+  //---------------------
+    const title = 'Landing Page'
+
+  //---------------------
+  //   QUERY DATA
+  //---------------------
+    
+
+  //---------------------
+  //   RENDER
+  //---------------------
+  return (
+    <div>
+        
+    </div>
+  )
+}
