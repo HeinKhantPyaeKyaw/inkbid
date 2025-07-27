8ba859be3448904f810b742c554b71767ec931e5
Jeff191997 <kaungmyatun19@gmail.com>
2025-07-27 17:48:09 +0700

    🐯feat: Finished BuyerDashboard Page UI, except Action Button from tables.

--- frontend/src/interfaces/buyer-dashboard-interface/buyer-dashboard-types.ts
+++ frontend/src/interfaces/buyer-dashboard-interface/buyer-dashboard-types.ts
@@ -1,5 +1,32 @@
+import { ArticleTableStatus, InventoryTableStatus } from './status-types';
+
 export interface InfoCardProps {
   cardTitle: string;
   cardDescription: string;
   icon: React.ElementType;
+}
+
+export interface ArticleTableItems {
+  id: number;
+  title: string;
+  yourBid: number;
+  currentBid: number;
+  timeRemaining: string; // ? DateTime
+  bidStatus: ArticleTableStatus | string;
+}
+
+export interface InventoryTableItems {
+  id: number;
+  title: string;
+  purchasedDate: string; // ? DateTime
+  contractPeriod: string; // ? DateTime
+  contractStatus: InventoryTableStatus | string;
+}
+
+export interface BuyerDashboardPaginationProps {
+  currentPage: number;
+  totalPages: number;
+  onPageChange: (page: number) => void;
+  onClickPreviousPage: () => void;
+  onClickNextPage: () => void;
 }
---
+++ frontend/src/interfaces/buyer-dashboard-interface/status-types/index.ts
@@ -0,0 +1,11 @@
+export enum ArticleTableStatus {
+  INPROGRESS = 'In Progress',
+  WON = 'Won',
+  LOST = 'Lost',
+  PENDING = 'Pending',
+}
+
+export enum InventoryTableStatus {
+  ACTIVE = 'Active',
+  EXPIRED = 'Expired',
+}
